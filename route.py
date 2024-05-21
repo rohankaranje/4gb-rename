d22225629af7c501bbbf64b906ea8619db6fc034
rohankaranje <149205870+rohankaranje@users.noreply.github.com>
2024-05-21 20:00:35 +0530

    Update route.py

--- route.py
+++ route.py
@@ -1,3 +1,4 @@
+# route.py
 from aiohttp import web
 
 routes = web.RouteTableDef()
@@ -6,8 +7,11 @@ routes = web.RouteTableDef()
 async def root_route_handler(request):
     return web.json_response("PyʀᴏBᴏᴛᴢ")
 
-
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
-    return web_app
+    runner = web.AppRunner(web_app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', 8080)  # Listen on all interfaces
+    await site.start()
+    print("======= Serving on http://0.0.0.0:8080/ =======")
